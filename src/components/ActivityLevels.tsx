93cc5f16eda31560af989314a9a67500809ba1db
Lewis Rotchell <rotchell92@gmail.com>
2021-08-08 14:50:29 +0100

    Added styling for activity level page

--- src/components/ActivityLevels.tsx
+++ src/components/ActivityLevels.tsx
@@ -9,21 +9,27 @@ type Props = {
 const ActivityLevels: React.FC<Props> = ({ show, height }) => {
   return (
     <section className={`${classes.activityLevels}`} style={{ height: height }}>
-      <h3 className="">Activity Levels</h3>
-      <div className="">
-        <p className="">Sedentary: little or no exercise, desk job</p>
+      <h2 className="">Activity Levels</h2>
+      <div className={classes.activityInfo}>
         <p className="">
-          Lightly Active: Light exercise or sports 1-3 days/week
+          <span className={classes.activityLabel}>Sedentary</span> Little or no
+          exercise, desk job.
         </p>
         <p className="">
-          Moderately Active: Moderate exercise or sports 6-7 days/week
+          <span className={classes.activityLabel}>Lightly Active</span> Light
+          exercise or sports 1-3 days/week.
         </p>
         <p className="">
-          Very Active: Hard exercise every day, or exercising 2 times/day
+          <span className={classes.activityLabel}>Moderately Active</span>{" "}
+          Moderate exercise or sports 6-7 days/week.
         </p>
         <p className="">
-          Extra Active: Very hard exercise/sports and physical job or train
-          twice daily
+          <span className={classes.activityLabel}> Very Active</span> Hard
+          exercise every day, or exercising 2 times/day.
+        </p>
+        <p className="">
+          <span className={classes.activityLabel}>Extra Active</span>
+          Very hard exercise/sports and physical job or train twice daily.
         </p>
       </div>
     </section>
